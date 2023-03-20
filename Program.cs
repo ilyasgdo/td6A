fadaa3a379ea3f5d2930e66d066768fe4457f7a7
ilyasgdo <ilyasghandaoui6@gmail.com>
2023-03-20 16:18:02 +0100

    ajout de l'opposé

--- Program.cs
+++ Program.cs
@@ -8,10 +8,17 @@ int produit(int a, int b)
     return a * b;
 }
 
+int oppose(int a)
+{
+    return -a;
+}
+
 #region main
 int nba = 9;
 int nbb = 31;
+int nbc = -7;
 Console.WriteLine($"la somme = {somme(nba, nbb)}");
 Console.WriteLine($"la multiplication = {produit(nba, nbb)}");
+Console.WriteLine($"l'opposé de {nbc} est : {oppose(nbc)}");
 #endregion
 
