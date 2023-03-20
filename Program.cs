123d48c3a6a69211c1af72ec73b2fd8559e34cd4
ilyasgdo <ilyasghandaoui6@gmail.com>
2023-03-20 16:14:43 +0100

    ajout de la mult

--- Program.cs
+++ Program.cs
@@ -3,10 +3,15 @@ int somme(int a,int b)
 {
     return a + b;
 }
+int produit(int a, int b)
+{
+    return a * b;
+}
 
 #region main
 int nba = 9;
 int nbb = 31;
 Console.WriteLine($"la somme = {somme(nba, nbb)}");
+Console.WriteLine($"la multiplication = {produit(nba, nbb)}");
 #endregion
 
